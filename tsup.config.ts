1b341eee95dc714622f3d33d7fdd1899ed8f0371
monakamon <>
2024-09-24 07:54:31 +0900

    init

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,45 @@
-import { defineConfig, Options } from 'tsup';
+import {defineConfig, Format, Options} from 'tsup';
 import { umdWrapper } from "esbuild-plugin-umd-wrapper";
+
+const defaultOutExtension: NonNullable<Options["outExtension"]> = ({
+    format,
+    pkgType
+}) => {
+    let jsExtension = ".js";
+    let dtsExtension = ".d.ts";
+    const isModule = pkgType === "module";
+    if (isModule && format === "cjs") {
+        jsExtension = ".cjs";
+        dtsExtension = ".d.cts";
+    }
+    if (!isModule && format === "esm") {
+        jsExtension = ".mjs";
+        dtsExtension = ".d.mts";
+    }
+    if (format === "iife") {
+        jsExtension = ".global.js";
+    }
+    return {
+        js: jsExtension,
+        dts: dtsExtension
+    };
+}
 
 export default defineConfig(() => {
     const format = ['cjs', 'esm', 'iife', 'umd'] as Options["format"];
     return {
         entry: ['src/index.ts'],
         format: format,
+        outExtension(params) {
+            if(params.format as Format === 'umd' as Format) {
+                return {
+                    js: '.umd.js',
+                    dts: ".d.ts"
+                };
+            }
+            return defaultOutExtension(params);
+        },
+        dts: true,
         esbuildPlugins: [
             umdWrapper({
                 libraryName: 'library-hoge',
