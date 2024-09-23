ef409e26d6d05926d402bb12b5fdb603465071ee
monakamon <>
2024-09-24 08:25:57 +0900

    init

--- packages/browser-sample/main.js
+++ packages/browser-sample/main.js
@@ -1,15 +1,15 @@
-import '../../libs/hoge/dist/index.js'
+
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 
-
+/*
 const hoge = new Hoge();
 hoge.sayHello();
 console.log(hoge.name);
-
+*/
 
 document.querySelector('#app').innerHTML = `
   <div>
