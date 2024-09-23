1b497a86a6d02083a6420fa4e1aeb65f9527036e
monakamon <>
2024-09-24 08:21:02 +0900

    init

--- packages/browser-sample/counter.js
+++ packages/browser-sample/counter.js
@@ -1,3 +1,9 @@
+import '../../libs/hoge/dist/index.js'
+
+const hoge = new Hoge();
+hoge.sayHello();
+console.log(hoge.name);
+
 export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
