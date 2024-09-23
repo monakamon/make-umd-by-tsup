121fd604b36c7dff760fbbbb278eada5311664a0
monakamon <>
2024-09-24 07:40:23 +0900

    init

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig, Options } from 'tsup';
+import { umdWrapper } from "esbuild-plugin-umd-wrapper";
 
 export default defineConfig(() => {
     const format = ['cjs', 'esm', 'iife', 'umd'] as Options["format"];
     return {
         entry: ['src/index.ts'],
         format: format,
+        esbuildPlugins: [
+            umdWrapper({
+                libraryName: 'library-hoge',
+            })
+        ],
         splitting: false,
         sourcemap: true,
         clean: true,
